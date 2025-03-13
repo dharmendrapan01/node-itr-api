fc4d9a05e308ff93d5ff37784b789c4ea1066f45
Dharmendra <dharmendrapandey71@gmail.com>
2025-03-13 10:56:43 +0530

    done soi

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const userRouter = require("./api/users/user.router");
 
+const userRouter = require("./api/users/user.router");
+const sourceRouter = require("./api/soi/soi.router");
+
+// Middleware to parse JSON requests
 app.use(express.json());
+
+// Use multiple routers
 app.use("/api/users", userRouter);
+app.use("/api/soi", sourceRouter);
+
 app.listen(process.env.APP_PORT, () => {
     console.log("Server is running on port : ", process.env.APP_PORT);
 });
